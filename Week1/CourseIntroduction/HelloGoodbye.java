a18a677ef1aeccf8efd65e18ce99a134344b02c9
Brian Mohammed Catraguna <briancatraguna@rocketmail.com>
2022-07-22 14:53:19 +0700

    [Fix] Solutions Naming

--- Week1/CourseIntroduction/HelloGoodbye.java
+++ Week1/CourseIntroduction/HelloGoodbye.java
@@ -1,12 +1,8 @@
 public class HelloGoodbye {
 
-    public static void main (String[] args) {
-        try {
-            System.out.println("Hello "+args[0]+" and "+args[1]);
-            System.out.println("Goodbye "+args[0]+" and "+args[1]);
-        } catch (Exception e) {
-            System.out.println("Not enough arguments are supplied!");
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0] + " and " + args[1]+".");
+        System.out.println("Goodbye " + args[1] + " and " + args[0]+".");
     }
 
 }
